c836f6ab3e58d1bcb4d902351b1decff0900601b
le5tef <freshmashine@gmail.com>
2022-05-17 23:18:47 +0300

    add redirect to calculator

--- src/router/index.js
+++ src/router/index.js
@@ -24,8 +24,14 @@ const routes = [
                 // this generates a separate chunk (about.[hash].js) for this route
                 // which is lazy-loaded when the route is visited.
                 component: () => import(/* webpackChunkName: "about" */ '../views/Currencies.vue')
+            },
+            {
+                path: '',
+                redirect() {
+                    return Trans.i18nRoute({name: "Calculator"});
+                }
             }
-        ]
+        ],
     },
     {
         path: '*',
